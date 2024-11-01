c83a92fb8ae32c923ad485a1156817c1927e8e7f
bquochtruong <brianquochungtruong@gmail.com>
2024-11-01 14:30:33 -0700

    Specify provider

--- environments/sandbox/backend.tf
+++ environments/sandbox/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = var.s3_state
     key            = "terraform-ec2-instance/dev/terraform.tfstate"
-    region         = var.region
+    region         = var.aws_region
     dynamodb_table = "your-terraform-lock-table"
     encrypt        = true
   }
--- environments/sandbox/provider.tf
+++ environments/sandbox/provider.tf
@@ -3,4 +3,6 @@
 # Specify the AWS provider
 provider "aws" {
   region = var.aws_region
+  iam_user_name = var.iam_user_name
+  s3_bucket_names = [var.s3_state, var.s3_demo]
 }
