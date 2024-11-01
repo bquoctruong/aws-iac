0f204ee8ccfda364f33764572806e78bedaeb8de
bquochtruong <brianquochungtruong@gmail.com>
2024-11-01 14:32:01 -0700

    Add specific bucket

--- environments/sandbox/provider.tf
+++ environments/sandbox/provider.tf
@@ -5,4 +5,6 @@ provider "aws" {
   region = var.aws_region
   iam_user_name = var.iam_user_name
   s3_bucket_names = [var.s3_state, var.s3_demo]
+  s3_state = var.s3_state
+  s3_demo = var.s3_demo
 }
