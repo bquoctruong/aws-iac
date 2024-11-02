347c32e6c2c59df1ba2463f9fc8c12b73dbe7243
bquochtruong <brianquochungtruong@gmail.com>
2024-11-02 09:53:25 -0700

    Update backend.tf

--- environments/sandbox/backend.tf
+++ environments/sandbox/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = var.s3_state
     key            = "terraform-ec2-instance/dev/terraform.tfstate"
-    region         = var.aws_region
     dynamodb_table = "your-terraform-lock-table"
     encrypt        = true
   }
